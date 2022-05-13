3d7d98694bc147257360f08410c3d99750421f55
nsshard <therealdude12@gmail.com>
2022-05-13 20:53:27 +0800

    buttons are made
    Buttons based in javascript have been added, using large/secondary bootstraps style. Also, new cool text logo was added on HTML (with a href link)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       helol
-    
+     
+function indexpage()
+
+
+
+
+
+
+
+<div class="indexbut">
+<button type="button"  onclick="indexpage()" class="btn btn-secondary btn-lg">Homepage</button>
+</div>
+
+<div class="browsebut">
+<button type="button"  onclick="browsepage()" class="btn btn-secondary btn-lg">Browse dogs</button>
+</div>
+
+<div class="loginbut">
+<button type="button"  onclick="login()" class="btn btn-secondary btn-lg">Login</button>
+</div>
+
+<div class="registerbut">
+<button type="button"  onclick="register()" class="btn btn-secondary btn-lg">Register</button>
+</div>
+
+
+
       </header>
     </div>
   );
