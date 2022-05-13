3762ee7f41478f6770804899db347e4d309188b9
nsshard <therealdude12@gmail.com>
2022-05-14 02:21:24 +0800

    new pages
    Added "browse dogs" (placeholder) and "register" pages. Both are nonfunctional as of now as I need to get the API working.

--- src/App.js
+++ src/App.js
@@ -47,10 +47,42 @@ function App() {
      
 	
 		
+<div class="signedinstuff">
+
+</div>
+
+<div class="browsedogsstuff">
+a
+</div>
 
 
+ <div class="registerstuff">
+<div class="registerform">
+<form>
+  
+    <div class="blacktext1">Username</div>
+	<br></br>
+	
+    <input type="text" name="Username" />
+	<br></br>
+<div class="blacktext2">Password</div>
+	<br></br>
+   <input type="text" name="Password" />
+   	<br></br>
+   <div class="blacktext2">Email</div>
+	<br></br>
+   <input type="text" name="Email" />
+   <br></br>
+   <div class="blacktext2">Staff code (optional)	</div>
+	<br></br>
+   <input type="text" name="Staff code" />
+   <br></br>
+  <input type="submit" value="Submit" />
+  
 
- 
+</form>
+</div>
+</div>
 
 
 
