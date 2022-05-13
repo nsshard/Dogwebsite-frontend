332b41204d08fc32ea3881d4dacf0a9c92d05423
nsshard <therealdude12@gmail.com>
2022-05-13 21:25:25 +0800

    index element completed
    Completed index page with a basic statement of the canine shelter, and some image+float text introducing the shelter's mission

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ function App() {
     <div className="App">
       <header className="App-header">
      
-function indexpage()
 
 
 
+<div class="homepagestuff">
 
+<div class="floattext1">
+WELCOME!
+</div>
+
+<div class="floattext2">
+The Canine Shelter of Hong Kong was formed in 1999, and our mission is formed on the basis that animals should receive the best welfare from our society, that's why we formed THE CANINE SHELTER. Here, you can browse our aavilable dogs and learn the information of each animal, and in which they are up to adoption! 
+</div>
+
+<img src="indexdog.jpg"> 
+</img>
+</div>
 
 
 
